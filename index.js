5d66af9613bec76fbf8ffa4cd5a83b8c2db54bd5
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-06-20 08:07:02 +0530

    added path dirname

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const path = require('path');
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -13,6 +15,8 @@ const { checkForAuthenticationCookie } = require('./middlewares/authentication.j
 const BlogModel = require('./models/blog.module.js');
 
 app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieparser())
 app.use(checkForAuthenticationCookie("token"))
